462abfbfd6fb2ec1a690dc29fd43764e20b54bbb
Bernhard Bablok <bablokb@gmx.de>
2018-05-28 17:45:42 +0200

    initial commit

---
+++ files/usr/local/bin/show_charset.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -----------------------------------------------------------------------------
+# This program prints the available charset to the display
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/simple-radio
+#
+# -----------------------------------------------------------------------------
+
+import sys, time
+
+I2C_PORT   = 1
+SLEEP_TIME = 5
+COLS       = 20
+ROWS       = 4
+
+try:
+  import lcddriver
+  lcd = lcddriver.lcd(port=I2C_PORT)
+  simulate = False
+except:
+  simulate = True
+
+if COLS == 20:
+  header = "    0123456789012345"
+else:  
+  header = "0123456789012345"
+
+for part in sys.argv[1:]:
+  part = int(part)
+  if COLS == 20:
+    line = "%3d:" % (part*16)
+  else:
+    line = ""
+  for nr in range(part*16,part*16+16):
+    line += (chr(nr))
+
+  # output header and content
+  if simulate:
+    print header
+    print line
+  else:
+    lcd.lcd_display_string(header,1)
+    lcd.lcd_display_string(line,2)
+
+  # and sleep
+  time.sleep(SLEEP_TIME)
